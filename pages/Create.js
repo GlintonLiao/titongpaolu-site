b02a03740de7a8c96b24a252e5765cdda8abe55b
Guotong Liao <815190191@qq.com>
2022-08-19 22:26:39 +0800

    fix: remove unused code

--- pages/Create.js
+++
@@ -1,8 +0,0 @@
-import swr from 'swr';
-
-const fetcher = (url) => fetch(url).then(res => res.json()).then(data => data);
-
-export default function Create() {
-  const { data, error } = swr('/api/create-page', fetcher)
-  return
-}
