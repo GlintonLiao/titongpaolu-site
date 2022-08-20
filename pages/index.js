9f1846bc41ca48ad3fa8185348b3a4a12f035fa3
Guotong Liao <815190191@qq.com>
2022-08-20 19:02:43 +0800

    feat: building home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react';
 import { queryDatabase } from './api/query-database';
 // import styles from '../styles/Home.module.css'
 
 export default function Home({database}) {
+
+  const arr = [false, false, false]
+
+  const [isHov, setIsHov] = useState(arr)
 
   const dataArr = database.results
   const obj = {
@@ -37,7 +42,13 @@ export default function Home({database}) {
     }
   })
 
-  console.info(view);
+  const handleMouse = (id) => {
+    const newHov = isHov
+    // console.log(isHov);
+    newHov[id] = !newHov[id]
+    setIsHov([...newHov])
+    console.log(isHov);
+  }
   
   return (
     <div>
@@ -50,14 +61,39 @@ export default function Home({database}) {
       <main className="max-w-2xl mx-auto">
         <div className='fixed inset-0 top-0 z-20 flex flex-col items-center justify-center text-lg'
         >
-          <div className='z-20 flex items-center space-x-6'>
-            <div className='transition duration-300 ease-in-out hover:-translate-y-2 relative text-white'>
-              <button className=' p-4 bg-gradient-to-t from-gray-950 to-grey-900 rounded-2xl shadow-highlight gruop'>
-                <div className='transition filter group-hover:drop-shadow-glow transform: none'>
+          <div className='z-20 flex text-sm items-center space-x-6'>
+            <div className='realtive'>
+              <button className='p-4 duration-300 ease-in-out hover:-translate-y-2 bg-gray-50 rounded-2xl shadow-lg group' onMouseEnter={() => handleMouse(0)} onMouseLeave={() => handleMouse(0)}>
+                <div className='transition filter group-hover:drop-shadow-glow'>
                   <div className='h-10 w-10'>
                   </div>
                 </div>
               </button>
+              <div className={`relative text-center transition ease-in-out duration-300 ${isHov[0] ? "opacity-100" : "opacity-0"}`}>
+                <p className={`transition duration-300 ${isHov[0] ? "" : "transform translate-y-2"}`}>图库</p>
+              </div>
+            </div>
+            <div className='realtive'>
+              <button className='p-4 duration-300 ease-in-out hover:-translate-y-2 bg-gray-50 rounded-2xl shadow-lg group' onMouseEnter={() => handleMouse(1)} onMouseLeave={() => handleMouse(1)}>
+                <div className='transition filter group-hover:drop-shadow-glow'>
+                  <div className='h-10 w-10'>
+                  </div>
+                </div>
+              </button>
+              <div className={`relative text-center transition ease-in-out duration-300 ${isHov[1] ? "opacity-100" : "opacity-0"}`}>
+                <p className={`transition duration-300 ${isHov[1] ? "" : "transform translate-y-2"}`}>时间线</p>
+              </div>
+            </div>
+            <div className='realtive'>
+              <button className='p-4 duration-300 ease-in-out hover:-translate-y-2 bg-gray-50 rounded-2xl shadow-lg group' onMouseEnter={() => handleMouse(2)} onMouseLeave={() => handleMouse(2)}>
+                <div className='transition filter group-hover:drop-shadow-glow'>
+                  <div className='h-10 w-10'>
+                  </div>
+                </div>
+              </button>
+              <div className={`relative text-center transition ease-in-out duration-300 ${isHov[2] ? "opacity-100" : "opacity-0"}`}>
+                <p className={`transition duration-300 ${isHov[2] ? "" : "transform translate-y-2"}`}>上传</p>
+              </div>
             </div>
           </div>
         </div>
@@ -66,17 +102,8 @@ export default function Home({database}) {
 
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+      <footer className='fixed inset-x-0 bottom-10 flex flex-col items-center justify-center text-md'>
+          Design and coded by Guotong Liao
       </footer>
     </div>
   )
