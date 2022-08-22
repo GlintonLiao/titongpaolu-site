181ad3bbb688056f64344e003f570f885325c1dc
Guotong Liao <37015336+GlintonLiao@users.noreply.github.com>
2022-08-22 13:08:15 +0800

    feat: create gallery page

---
+++ pages/gallery.js
@@ -0,0 +1,14 @@
+import React, {useContext} from 'react'
+
+const gallery = () => {
+  // const data = useContext();
+  return (
+    <div>
+      {imgs.map(img, _idx) => (
+        <Image />
+      )}
+    </div>
+  )
+}
+
+export default gallery
