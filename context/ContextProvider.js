1919efefffd138364dc0551b62c42afac7be3588
Guotong Liao <815190191@qq.com>
2022-08-24 22:03:42 +0800

    fix: null array

--- context/ContextProvider.js
+++ context/ContextProvider.js
@@ -3,8 +3,7 @@ import React, { useState } from "react";
 export const Context = createContext();
 
 function ContextProvider({ children }) {
-  const [posts, setPosts] = useState({});
-  posts = []
+  const [posts, setPosts] = useState([]);
   return (
     <Context.Provider value={[posts, setPosts]}>
       {children}
